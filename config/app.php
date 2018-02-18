0c3d786927547ef2f05b73a0c767f3aa23000ff4
Chukwuemeka Ihedoro <caihedoro@gmail.com>
2018-02-18 09:26:16 +0100

    removed extraneous files with minor changes

--- config/app.php
+++ config/app.php
@@ -231,8 +231,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
-        'Debugbar' => Barryvdh\Debugbar\Facade::class,
-
     ],
 
 ];
